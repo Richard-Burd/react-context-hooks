5fe9af3ab1567341d97bdba2a0370bb66c10f6c9
Richard-Burd <rick.a.burd@gmail.com>
2022-02-09 10:07:24 -0800

    Lesson 8 Completed

---
+++ contextapp/src/components/NavBar(old).js
@@ -0,0 +1,52 @@
+import React, {  Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+
+// this method uses a context consumer instead of the ".Provider" 
+// method as shown in the "OldNavbar" component below.
+// The former can be used on both class and functional components 
+// whereas the latter only works for functional components
+class Navbar extends Component {
+  render() {
+    return (
+      <ThemeContext.Consumer>{(context) => {
+        const { isLightTheme, light, dark } = context;
+        const theme = isLightTheme ? light : dark;
+        return(
+          <nav style={{ background: theme.ui, color: theme.syntax }}>
+            <h1>Context App</h1>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        )
+      }}</ThemeContext.Consumer>
+
+    );
+  }
+}
+
+// this is one way to consume context by using the static property called 'contextType',
+// and setting it equal to whatever context we want to use (ThemeContext)
+// this only works with class components 
+class OldNavbar extends Component {
+  static contextType = ThemeContext;
+  render() {
+    // console.log(this.context)
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+    return (
+      <nav style={{ background: theme.ui, color: theme.syntax }}>
+        <h1>Context App</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    );
+  }
+}
+ 
+export default Navbar;
--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -1,52 +1,34 @@
 import React, {  Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
 
-// this method uses a context consumer instead of the ".Provider" 
-// method as shown in the "OldNavbar" component below.
-// The former can be used on both class and functional components 
-// whereas the latter only works for functional components
 class Navbar extends Component {
   render() {
     return (
-      <ThemeContext.Consumer>{(context) => {
-        const { isLightTheme, light, dark } = context;
-        const theme = isLightTheme ? light : dark;
-        return(
-          <nav style={{ background: theme.ui, color: theme.syntax }}>
-            <h1>Context App</h1>
-            <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
-          </nav>
-        )
-      }}</ThemeContext.Consumer>
-
+      <AuthContext.Consumer>{(authContext) => (
+        <ThemeContext.Consumer>{(themeContext) => {
+          const { isAuthenticated, toggleAuth } = authContext;
+          const { isLightTheme, light, dark } = themeContext;
+          const theme = isLightTheme ? light : dark;
+          return(
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
+              <h1>Context App</h1>
+              <div onClick={toggleAuth}>
+                { isAuthenticated ? 'Logged in' : 'Logged out' }
+              </div>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          )
+        }}</ThemeContext.Consumer>
+      )}
+      
+      </AuthContext.Consumer>
     );
   }
 }
 
-// this is one way to consume context by using the static property called 'contextType',
-// and setting it equal to whatever context we want to use (ThemeContext)
-// this only works with class components 
-class OldNavbar extends Component {
-  static contextType = ThemeContext;
-  render() {
-    // console.log(this.context)
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    return (
-      <nav style={{ background: theme.ui, color: theme.syntax }}>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    );
-  }
-}
- 
 export default Navbar;
