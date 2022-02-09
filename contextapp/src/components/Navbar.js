bd7d443ad1c759a8b3a2f4b05040525ce1659732
Richard-Burd <rick.a.burd@gmail.com>
2022-02-09 09:01:11 -0800

    context consumer vs. context provider

--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -1,12 +1,38 @@
 import React, {  Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
+// this method uses a context consumer instead of the ".Provider" 
+// method as shown in the "OldNavbar" component below.
+// The former can be used on both class and functional components 
+// whereas the latter only works for class components
 class Navbar extends Component {
   static contextType = ThemeContext;
   render() {
     console.log(this.context)
     const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark
+    const theme = isLightTheme ? light : dark;
+    return (
+      <nav style={{ background: theme.ui, color: theme.syntax }}>
+        <h1>Context App</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+// this is one way to consume context by using the static property called 'contextType',
+// and setting it equal to whatever context we want to use (ThemeContext)
+// this only works with class components 
+class OldNavbar extends Component {
+  static contextType = ThemeContext;
+  render() {
+    // console.log(this.context)
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
     return (
       <nav style={{ background: theme.ui, color: theme.syntax }}>
         <h1>Context App</h1>
