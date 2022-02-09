8fdf82fed78360b6fcaf6a61f08ba101b40fe698
Richard-Burd <rick.a.burd@gmail.com>
2022-02-09 09:13:36 -0800

    Lesson 6 Complete

--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -4,22 +4,25 @@ import { ThemeContext } from '../contexts/ThemeContext';
 // this method uses a context consumer instead of the ".Provider" 
 // method as shown in the "OldNavbar" component below.
 // The former can be used on both class and functional components 
-// whereas the latter only works for class components
+// whereas the latter only works for functional components
 class Navbar extends Component {
-  static contextType = ThemeContext;
   render() {
-    console.log(this.context)
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <nav style={{ background: theme.ui, color: theme.syntax }}>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>{(context) => {
+        const { isLightTheme, light, dark } = context;
+        const theme = isLightTheme ? light : dark;
+        return(
+          <nav style={{ background: theme.ui, color: theme.syntax }}>
+            <h1>Context App</h1>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        )
+      }}</ThemeContext.Consumer>
+
     );
   }
 }
